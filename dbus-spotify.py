b183e87349bc9d40c3b348446ad53649ea3df185
Alve Svarén <alve@hotmail.se>
2020-05-14 10:48:15 +0200

    Updated code organization and added some basic documentation

--- dbus-spotify.py
+++ dbus-spotify.py
@@ -1,37 +1,71 @@
 import dbus
 
+
+
+
+
 class Spotify:
+    """
+    A spotify dbus client object to control spotify from python.
+    """
+
+    class Track:
+        """
+        An object representing a spotify track
+        """
+        def __init__(self, *args, title=None, artist=None, art_uri=None):
+            self.title = str(title)
+            self.artist = str(artist)
+            self.art_uri = str(art_uri)
+
     def __init__(self):
         self._bus = dbus.SessionBus()
-        self._obj = self._bus.get_object("org.mpris.MediaPlayer2.spotify","/org/mpris/MediaPlayer2")
-        self._spotify_iface = dbus.Interface(self._obj, dbus_interface="org.mpris.MediaPlayer2.Player")
-        self._props_iface = dbus.Interface(self._obj, dbus_interface="org.freedesktop.DBus.Properties")
+        self._obj = self._bus.get_object(
+            "org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+        self._spotify_iface = dbus.Interface(
+            self._obj, dbus_interface="org.mpris.MediaPlayer2.Player")
+        self._props_iface = dbus.Interface(
+            self._obj, dbus_interface="org.freedesktop.DBus.Properties")
 
     def get_metadata(self) -> dbus.Dictionary:
+        """
+        Returns the raw dbus metadata.
+        """
         return self._props_iface.GetAll("org.mpris.MediaPlayer2.Player")["Metadata"]
 
     def play(self):
+        """
+        Tells spotify to start playing.
+        """
         self._spotify_iface.Play()
 
     def play_pause(self):
+        """
+        Tells spotify to toggle playing.
+        """
         self._spotify_iface.PlayPause()
 
     def pause(self):
+        """
+        Tells spotify to pause.
+        """
         self._spotify_iface.Pause()
 
     def stop(self):
+        """
+        Tells spotify to stop playing.
+        """
         self._spotify_iface.Stop()
 
     @property
-    def track(self) -> tuple:
+    def track(self) -> Track:
+        """
+        Returns a track object containing current track data.
+        """
         metadata = self.get_metadata()
-        return str(metadata["xesam:title"]), str(metadata["xesam:artist"][0])
-    
-    @property
-    def art_uri(self):
-        return self.get_metadata()["mpris:artUrl"]
+        return self.Track(title=metadata["xesam:title"], artist=metadata["xesam:artist"][0])
 
 
 if __name__ == "__main__":
     spotify = Spotify()
-    print("Currently playing:", spotify.track[0], "-", spotify.track[1])
+    print("Currently playing:", spotify.track.title, "-", spotify.track.artist)
